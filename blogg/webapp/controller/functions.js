457ca1aa52721c7ca2e872d48b6a13ee72816c72
Jashbet Val <chivas.ja@live.com.mx>
2022-08-12 04:57:26 +0000

    Diseno de index.html

---
+++ blogg/webapp/controller/functions.js
@@ -0,0 +1,36 @@
+var num1 = document.getElementById=("num1");
+var num2 = document.getElementById=("num2");
+//var t2 = document.getElementById("t2");
+
+function suma(num1, num2){
+    var res=num1+num2;
+    console.log(res);
+    return res;
+}
+
+function resta(num1, num2){
+    var res=num1-num2;
+    console.log(res);
+    return res;
+}
+
+function multiplicacion(num1, num2){
+    var res=num1*num2;
+    console.log(res);
+    return res;
+}
+
+function division(num1, num2){
+    var res=num1/num2;
+    console.log(res);
+    return res;
+}
+
+var opc
+
+/* Selecting DOM element */
+const button = document.addEventListener();
+ 
+
+/* Event listener */
+button1.addEventListener("click", suma, false);
---
+++ blogg/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["blogg/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
