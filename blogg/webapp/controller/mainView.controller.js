8792f6f093c25efd7f06135c7f31d4588619964d
JashbetVal <chivas.ja@live.com.mx>
2022-08-16 02:47:08 +0000

    inicio  de funciones de vistas

--- blogg/webapp/controller/mainView.controller.js
+++ blogg/webapp/controller/mainView.controller.js
@@ -1,15 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,MessageToast ) {
         "use strict";
 
         return Controller.extend("blogg.controller.mainView", {
+           
             onInit: function () {
-
-            }
+            console.log("HOLA");
+            
+           
+            },//Fin de onInit
+            
+            onPress: function (evt) {
+                MessageToast.show(evt.getSource().getId() + " Pressed");
+                console.log("QUE ONDAAAAAAAAAAA");
+                var num1 = documento.getElementById("input0");
+                console.log(num1.value);
+                
+         
+            },  //Fin de on Press
         });
-    });
+    }
+);
+
